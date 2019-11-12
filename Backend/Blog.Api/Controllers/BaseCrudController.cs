948f14274406dbed73deff3026ff953f6b494b23
Ali Mousavi Kherad <alimousavikherad@gmail.com>
2019-11-12 12:17:38 +0330

    Init

---
+++ Backend/Blog.Api/Controllers/BaseCrudController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseCrudController<TQueryModel, TCommandModel, TKey, TEntity, TService> : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task Get(string id, CancellationToken cancellationToken)
+        {
+            
+        }
+        
+        [HttpGet]
+        public async Task GetAll(CancellationToken cancellationToken)
+        {
+            
+        }
+        
+        [HttpPost]
+        public async Task Insert(CancellationToken cancellationToken)
+        {
+            
+        }
+        
+        [HttpPut("{id}")]
+        public async Task Update(string id, CancellationToken cancellationToken)
+        {
+            
+        }
+        
+        [HttpPut("{id}")]
+        public async Task UpdatePartial(string id, CancellationToken cancellationToken)
+        {
+            
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task Delete(string id, CancellationToken cancellationToken)
+        {
+            
+        }
+    }
+}
